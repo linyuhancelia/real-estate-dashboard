3915e9ee03345f07190d0e2a1d9ed67af34481b0
linyuhancelia <linyuhan@hotmail.com>
2026-07-16 18:57:22 +0800

    v5.0: real anjuke data + bottom tab methodology

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'retracker-v3';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+
+self.addEventListener('fetch', e => {
+  // Network first for data, cache first for assets
+  if (e.request.url.includes('latest.json')) {
+    e.respondWith(fetch(e.request).then(r => {
+      const rc = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, rc));
+      return r;
+    }).catch(() => caches.match(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
